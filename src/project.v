90103ceea36117f95460c4b5beb04c052dcf94ee
sriram829 <sriram@ece.sastra.edu>
2026-04-18 07:59:09 +0530

    Create project.v

---
+++ src/project.v
@@ -0,0 +1,26 @@
+`default_nettype none
+
+module tt_um_fp_id (
+    input  wire [7:0] ui_in,
+    output wire [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe,
+    input  wire       ena,
+    input  wire       clk,
+    input  wire       rst_n
+);
+
+    // Include your actual top module
+    tt_um_fp_id_impl impl (
+        .ui_in   (ui_in),
+        .uo_out  (uo_out),
+        .uio_in  (uio_in),
+        .uio_out (uio_out),
+        .uio_oe  (uio_oe),
+        .ena     (ena),
+        .clk     (clk),
+        .rst_n   (rst_n)
+    );
+
+endmodule
